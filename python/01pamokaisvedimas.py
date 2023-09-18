7e1eb338ecd0709a61ce4d9c2f161d39c2c41393
Moksleivis <Moksleivis@kitm.local>
2023-09-18 08:23:49 +0300

    python

---
+++ python/01pamokaisvedimas.py
@@ -0,0 +1,11 @@
+print('Pirmas kartas')
+print("Antras kartas")
+print("I'm \"best\" programmer")
+print('tekstas \nper \nkelias \neilutes')
+print('tekstas', end=' ')
+print('Vienoje', end=' ')
+print('eiluteje', end='........')
+print('pirmas'+'antras')
+print('pirmas','antras')
+#Komentaras
+
